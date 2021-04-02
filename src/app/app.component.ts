1cc4fa86d94dc713ddb6944a4b762e6fac213ed1
Paulo Almeida <pauloalmeida.rj@gmail.com>
2021-04-02 10:11:15 -0300

    versao inicial do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as services from '../services/authServices';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appClientes';
+
+export class AppComponent implements OnInit {
+  
+  //atributo
+  usuarioAutenticado = false;
+
+  ngOnInit() : void {
+    //verificar se o usuario está ou não autenticado..
+    this.usuarioAutenticado = services.getToken() !== null;
+  }
+
+  logout() : void {
+
+    //verificando se o usuario deseja sair do sistema
+    if(window.confirm('Deseja encerrar sua sessão?')){
+
+      //destruir o token do usuario..
+      services.removeToken();
+
+      //redirecionar de volta para a página inicial (login)
+      services.redirectToLoginPage();
+    }
+
+  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl : 'http://clienteapp1-001-site1.itempurl.com' //Endereço da API..
 };
 
 /*
